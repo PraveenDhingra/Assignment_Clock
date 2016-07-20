e618b66cd87ec2e7f74f56ee9b3d37e793d48e73
Praveen Kumar1 <praveen.kumar@QAIT.COM>
2016-07-20 11:55:08 +0530

    Added a assignment

---
+++ Angle.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+
+public class Angle {
+	
+	public static void main(String[] args) {
+	System.out.println("Enter the time in HH:MM format");
+	Scanner value = new Scanner(System.in);
+	String InputTime = value.nextLine();
+	value.close();
+	System.out.println(InputTime);
+	String[] splitvalue = InputTime.split(":");
+	int hour= Integer.parseInt(splitvalue[0]);
+	int Minute= Integer.parseInt(splitvalue[1]);
+	
+	double HourHandPosition,MinuteHandPosition;
+	HourHandPosition=hour*30 + Minute*0.5 ;
+	MinuteHandPosition= Minute * 6;
+	
+	double resultAngle= Math.abs(HourHandPosition-MinuteHandPosition);
+	if (resultAngle > 180){
+		resultAngle=360-resultAngle;
+	}
+		System.out.println("The angle is: "+resultAngle);
+		
+	}
+				
+	}
